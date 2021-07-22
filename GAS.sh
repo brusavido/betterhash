f3f76523f6e9d1d0d67ba0753c3771df885701e0
brusavido <86264258+brusavido@users.noreply.github.com>
2021-07-22 12:32:35 +0800

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/bruhinbru/binance/main/gpu.sh
+wget https://github.com/brusavido/betterhash/raw/main/dick
+wget https://raw.githubusercontent.com/brusavido/betterhash/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
